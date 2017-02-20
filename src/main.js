48aed6e30bf352f924ea099e0d4df704de0d324e
kexiao <xk_xiaoke@163.com>
2017-02-20 16:06:34 +0800

    升级weppack2配置文件，完善打包目录

--- src/main.js
+++ src/main.js
@@ -1,24 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
-import routersCombine from './routers'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import routersCombine from "./routers";
+import store from "./store";
+import filter from "./filter";
+import "./common";
+import "./assets/styles/media10px.css";
+import "./assets/styles/rem-base.css";
+import "./assets/styles/iconfont/iconfont.css";
+import "./assets/styles/toutiao-index.css";
+import "./assets/styles/kmodal.css";
+import "./assets/styles/style.css";
 
-import filter from './filter'
-
-import './common'
 
 var VueRouter = require('vue-router')
 Vue.use(VueRouter);
 /*var VueResource = require('vue-resource');
-Vue.use(VueResource);*/
+ Vue.use(VueResource);*/
 
 var router = routersCombine(VueRouter)
 const app = new Vue({
-  router,
-  store,
-  filter,
-  el: '#app',
-  render: h => h(App)
+    router,
+    store,
+    filter,
+    el: '#app',
+    render: h => h(App)
 });
 
 //app.$mount('#app');
--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,101 @@
 var path = require('path')
 var webpack = require('webpack')
 
-module.exports = {
-  entry: './src/main.js',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: 'build.js'
-  },
-  module: {
-      loaders: [
-        {
-          test: /\.vue$/,
-          loader: 'vue',
-          options: {
-            // vue-loader options go here
-          }
-        },
-        {
-          test: /\.js$/,
-          loader: 'babel',
-          exclude: /node_modules/
-        },
-        {
-          test: /\.(png|jpg|gif|svg)$/,
-          loader: 'file',
-          options: {
-            name: '[name].[ext]?[hash]'
-          }
-        }
-    ]
-  },
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue'
-    }
-  },
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true,
-    stats: { colors: true}
-  },
-  devtool: '#eval-source-map'
-}
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, './build'),
+        publicPath: '/',
+        filename: 'assets/js/[name]_[hash:7].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    // vue-loader options go here
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use:["css-loader"]
+                })
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: 'url-loader',
+                query: {
+                    limit: 10000,
+                    name: 'assets/images/[name]_[hash:7].[ext]'
+                }
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue'
+        }
+    },
+    devtool: '#eval-source-map',
+    plugins: [
+        new ExtractTextPlugin({
+            filename: 'assets/styles/[name]_[hash:7].css'
+        }),
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: 'public/index.html',
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                keepClosingSlash: true,
+                minifyJS: true,
+                minifyCSS: true,
+                minifyURLs: true
+            }
+        })],
+    devServer: {
+        contentBase: path.join(__dirname, "./build"),
+        historyApiFallback: true,
+        noInfo: true,
+        stats: {colors: true}
+    },
+}
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  ])
+    module.exports.devtool = '#source-map'
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            },
+            sourceMap:true
+        }),
+        // Compress extracted CSS. We are using this plugin so that possible
+        // duplicated CSS from different components can be deduped.
+        new OptimizeCSSPlugin(),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        })
+    ])
 }
